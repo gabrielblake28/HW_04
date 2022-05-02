61d788f72783278f85a9cbb3a13c61ee6328449e
Gabriel Blake <gabriel.blake28@gmail.com>
2022-05-01 21:35:32 -0700

    initial html/javascript

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-let origArray = [2, 3, 4];
+let foodArray = [];
+
+let Food = function (pname, pcalories) {
+  this.name = pname;
+  this.calories = pcalories;
+};
 
 document.addEventListener("DOMContentLoaded", function (event) {
-  document.getElementById("firstInput").value = origArray[0];
-  document.getElementById("secondInput").value = origArray[1];
-  document.getElementById("thirdInput").value = origArray[2];
+  document.getElementById("addfoodBtn");
 
   document.getElementById("btnTwo").addEventListener("click", function () {
     const cubedArray = origArray.map((x) => x * x * x);
@@ -15,19 +18,3 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
   document.getElementById("btnThree").addEventListener("click", toTheFourth);
 });
-
-function squared() {
-  const squaredArray = origArray.map((x) => x * x);
-
-  document.getElementById("firstInput").value = squaredArray[0];
-  document.getElementById("secondInput").value = squaredArray[1];
-  document.getElementById("thirdInput").value = squaredArray[2];
-}
-
-function toTheFourth() {
-  const toTheFourthArray = origArray.map((x) => x * x * x * x);
-
-  document.getElementById("firstInput").value = toTheFourthArray[0];
-  document.getElementById("secondInput").value = toTheFourthArray[1];
-  document.getElementById("thirdInput").value = toTheFourthArray[2];
-}
